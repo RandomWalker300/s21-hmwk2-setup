08f32dfb6b036c92e1e07e21c3c00520b0f541b9
XuhengLi <xl2784@columbia.edu>
2019-09-17 12:49:10 -0400

    add setup script for emulator

---
+++ emu_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+TMP_DIR=".w4118_hmwk2_setup_tmp"
+SDK_DIR="$HOME/Android/Sdk"
+
+cd $HOME
+
+if [ ! -d "Android" ]; then
+	mkdir Android
+	mkdir Android/Sdk
+elif [ ! -d "Android/Sdk" ]; then
+	mkdir Android/Sdk
+fi
+
+if [ ! -d ".android" ]; then
+	mkdir .android
+	touch .android/repositories.cfg 
+elif [ ! f ".android/repositories.cfg" ]; then
+	touch .android/repositories.cfg 
+fi
+
+cd Android/Sdk
+
+# Download Android Sdk
+wget https://dl.google.com/android/repository/sdk-tools-linux-4333796.zip
+unzip -o sdk-tools-linux-4333796.zip
+# Downlaod Andoird platform tool
+wget https://dl.google.com/android/repository/platform-tools-latest-linux.zip
+unzip -o platform-tools-latest-linux.zip
+
+# Setup sdk
+yes "y" | ./tools/bin/sdkmanager --install 'ndk;20.0.5594570'
+yes "y" | ./tools/bin/sdkmanager 'platforms;android-29'
+yes "y" | ./tools/bin/sdkmanager 'system-images;android-29;default;x86_64'
+# Set AVD
+yes "" | ./tools/bin/avdmanager create avd -k 'system-images;android-29;default;x86_64' -n w4118_emu
+
---
+++ run_emulator.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+EMULATOR_NAME="w4118_emu"
+NO_SNAP="-no-snapshot"
+SHOW_KERNEL=""
+KERNEL=""
+EMULATOR_PATH="$HOME/Android/Sdk/tools/emulator"
+NO_WINDOW=""
+RAM=""
+
+usage()
+{
+	echo "usage:	run_emulator [-p emulator_path][-n emulator_name] [-m RAM_SIZE][-k kernel_path] [-s] [-c]"
+	echo "	emulator_path default to $EMULATOR_PATH."
+	echo "	emulator_name default to $EMULATOR_NAME."
+	echo "	-m: set the RAM size in MB from 128-4096, defualt to 2048,"
+	echo "	    reduece this if your emulator is too slow."
+	echo "	-k: specify a kernel image."
+	echo "	    When you specify a kernel image, the -s option will be enabled"
+	echo "	-s: show kernel log infomation."
+	echo "	-c: command line only, use this if your emulator is too slow"
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-k | --kernel )	shift
+				KERNEL=$1
+				SHOW_KERNEL="-show-kernel"
+				;;
+		-e | --emulator ) shift
+				EMULATOR_PATH=$1
+				;;
+		-n | --name ) 	shift
+				EMULATOR_NAME=$1
+				;;
+		-s | --show-kernel )
+				SHOW_KERNEL="-show-kernel"
+				;;
+		-c | --no-window )
+				NO_WINDOW="-no-window"
+				;;
+		-m | --memory ) shift
+				RAM="-memory $1"
+				;;
+		-h | --help | * )	usage
+					exit
+					;;
+	esac
+	shift
+done
+
+$EMULATOR_PATH @$EMULATOR_NAME $NO_SNAP $SHOW_KERNEL $KERNEL $NO_WINDOW
